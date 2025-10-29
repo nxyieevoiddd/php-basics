2ff6ba368b1fb1d41159597ae8425c6c1429d69a
nxyieee <bindris.olivers@gmail.com>
2025-10-29 10:14:37 +0200

    Create for-loop.php

---
+++ for-loop.php
@@ -0,0 +1,10 @@
+<?php
+
+// 4. uzdevums: 'for' cikls
+//a) Izveidojiet failu "for-loop.php";
+//b) Izveido 'for' ciklu, kas izvada veselus skaitļus no 7 līdz 16;
+//c) Versionējiet 4.uzd. atsevišķā komitā;
+
+for ($x = 7; $x <= 16; $x++) {
+  echo "The number is: $x ";
+}
