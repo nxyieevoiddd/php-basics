2702b480654efddd41eab0866458c30ce81ab261
DeivisStasuls <deivis.stasuls@gmail.com>
2025-10-29 09:34:16 +0200

    first commit

---
+++ output.php
@@ -0,0 +1,11 @@
+<?php
+
+// 1. uzdevums: Vides sagatavošana
+// a) Iztīriet sava vārda mapi tukšu;
+// b) Atveriet vārda mapi Terminal aplikācijā;
+// c) Izveidojiet mapi "php-basics";
+// d) Atveriet mapes saturu ar VS Code;
+// e) Izveidojiet failu "output.php", kurā ir koda rinda, kas terminālī izvada Tavu vārdu;
+// f) Izpildiet šo failu ar "php" programmu un pārliecinaties, ka vārds ir izvadīts;
+
+echo "Olivers";
